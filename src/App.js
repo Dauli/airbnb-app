6fef172c0ad1098ae441afb1d0bf52265eb1eb03
dauli <alexisangedauli@gmail.com>
2023-01-22 20:33:47 +0200

    Fixing Card component to use props - 1

--- src/App.js
+++ src/App.js
@@ -3,14 +3,41 @@ import Navbar from './component/Navbar';
 import Hero from './component/Hero';
 import Experience from './component/Experience';
 import Card from './component/Card';
+import Katie from "./images/katie-zaferes.png"
+import Alex from "./images/AlexisDauli.jpg"
+import Divine from "./images/Divine.jpg"
+
 
 function App() {
   return (
     <div className="App">
+      
       <Navbar />
       <Hero />
       <Experience />
-      <Card />
+
+      <div className='cards'>
+        <Card 
+          img={Katie}
+          ratingScore="4.0"
+          title="Life lessons with Kent"
+          price="200"
+        />
+
+        <Card 
+          img={Alex}
+          ratingScore="5.0"
+          title="Life lessons with Kent"
+          price="2500"
+        />
+
+        <Card 
+          img={Divine}
+          ratingScore="4.5"
+          title="Life lessons with Kent"
+          price="1500"
+        />
+      </div>
     </div>
   );
 }
--- src/component/Card.js
+++ src/component/Card.js
@@ -1,46 +1,18 @@
-import Person from "../images/katie-zaferes.png"
 import Star from "../images/star.png"
 
-function Card() {
+function Card(props) {
     return(
-        <div className="card-container">
-            <section className="card1">
-                <img className="person-image" src={Person} alt="person" />
-                <p className="star">
-                    <img src={Star} alt="star" className="star-pic"/>
-                    <span>5.0 (6) USA</span>
-                </p>
-                <p className="title">Life lessons with Kent</p>
-                <p className="cost">
-                    <span className="span-price">From R500</span> / person
-                </p>
-            </section>
-
-            <section className="card2">
-                <img className="person-image" src={Person} alt="person" />
-                <p className="star">
-                    <img src={Star} alt="star" className="star-pic"/>
-                    <span>5.0 (6) USA</span>
-                </p>
-                <p className="title">Life lessons with Kent</p>
-                <p className="cost">
-                    <span className="span-price">From R500</span> / person
-                </p>
-            </section>
-
-            <section className="card3">
-                <img className="person-image" src={Person} alt="person" />
-                <p className="star">
-                    <img src={Star} alt="star" className="star-pic"/>
-                    <span>5.0 (6) USA</span>
-                </p>
-                <p className="title">Life lessons with Kent</p>
-                <p className="cost">
-                    <span className="span-price">From R500</span> / person
-                </p>
-            </section>
-        </div>
-        
+        <section className="card-container">
+            <img className="person-image" src={props.img} alt="person" />
+            <p className="star">
+                <img src={Star} alt="star" className="star-pic"/>
+                <span>{props.ratingScore} (6) USA</span>
+            </p>
+            <p className="title">Life lessons with Kent</p>
+            <p className="cost">
+                <span className="span-price">From {props.price}</span> / person
+            </p>
+        </section>        
     )
 }
 
